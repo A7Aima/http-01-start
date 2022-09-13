794bf330d9b5e139fbcde3402a9df92b5cd6e1ee
A7Aima <aimaabhilash@gmail.com>
2022-09-13 13:10:50 +0530

    error handeling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 import { Post } from './post.model';
 import { NgForm } from '@angular/forms';
 import { PostService } from './post.service';
@@ -8,33 +8,52 @@ import { PostService } from './post.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
-  @ViewChild("postForm") form: NgForm;
+  @ViewChild('postForm') form: NgForm;
   isFetching = false;
+  error = null;
+  private errorListner: Subscription;
 
-  constructor(private http: HttpClient, private postService: PostService) { }
+  constructor(private http: HttpClient, private postService: PostService) {}
+
+  ngOnDestroy(): void {
+    this.errorListner.unsubscribe();
+  }
 
   ngOnInit() {
+    this.errorListner = this.postService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
     this.onFetchPosts();
   }
 
-  url: string = 'https://http-angular-test-c4fd2-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+  url: string =
+    'https://http-angular-test-c4fd2-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json';
   onCreatePost(postData: Post) {
     // Send Http request
-    this.postService.createAndStorePost(postData.title, postData.content, this.url);
+    this.postService.createAndStorePost(
+      postData.title,
+      postData.content,
+      this.url
+    );
     this.form.reset();
   }
 
   onFetchPosts() {
     this.isFetching = true;
-    this.postService.fetchPost(this.url)
-      .subscribe(body => {
+    this.postService.fetchPost(this.url).subscribe(
+      (body) => {
         this.isFetching = false;
         this.loadedPosts = body;
-      });
+      },
+      (error) => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
     // Send Http request
   }
 
@@ -44,5 +63,7 @@ export class AppComponent implements OnInit {
       this.loadedPosts = [];
     });
   }
-
+  onHandleError() {
+    this.error = null;
+  }
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,39 +1,49 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Post } from "./post.model";
-import { map } from "rxjs";
+import { Post } from './post.model';
+import { Subject, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-
 export class PostService {
+  error = new Subject<string>();
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    createAndStorePost(title: string, content: string, url: string) {
-        const postData: Post = { title: title, content: content };
-        console.log(postData);
-        this.http.post<{ name: string }>(url, postData).subscribe(responseData => {
-            console.log(responseData);
-        });
-    }
+  createAndStorePost(title: string, content: string, url: string) {
+    const postData: Post = { title: title, content: content };
+    console.log(postData);
+    this.http.post<{ name: string }>(url, postData).subscribe(
+      (responseData) => {
+        console.log(responseData);
+      },
+      (error) => {
+        this.error.next(error.message);
+      }
+    );
+  }
 
-    fetchPost(url: string) {
-        // this request is sending the observable but doesn't get called until it get subscribed
-        return this.http.get<{ [key: string]: Post }>(url)
-            .pipe(map(responseData => {
-                const postArray: Post[] = [];
-                for (const key in responseData) {
-                    if (responseData.hasOwnProperty(key)) {
-                        postArray.push({ ...responseData[key], id: key });
-                    }
-                }
-                return postArray;
-            }));
-    }
+  fetchPost(url: string) {
+    // this request is sending the observable but doesn't get called until it get subscribed
+    return this.http.get<{ [key: string]: Post }>(url).pipe(
+      map((responseData) => {
+        const postArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postArray;
+      }),
+      catchError((errorRes) => {
+        return throwError(errorRes);
+      })
+    );
+  }
 
-    deletePost(url: string) {
-        return this.http.delete(url);
-    }
+  deletePost(url: string) {
+    return this.http.delete(url);
+  }
 }
