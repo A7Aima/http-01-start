65a483c55185c9d3b2e0e5e32581e7264e36a6a1
A7Aima <aimaabhilash@gmail.com>
2022-09-13 16:40:55 +0530

    headers

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from './post.model';
 import { Subject, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
@@ -27,20 +27,26 @@ export class PostService {
 
   fetchPost(url: string) {
     // this request is sending the observable but doesn't get called until it get subscribed
-    return this.http.get<{ [key: string]: Post }>(url).pipe(
-      map((responseData) => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({ ...responseData[key], id: key });
-          }
-        }
-        return postArray;
-      }),
-      catchError((errorRes) => {
-        return throwError(errorRes);
+    return this.http
+      .get<{ [key: string]: Post }>(url, {
+        headers: new HttpHeaders({
+          'Custom-Header': 'Hello', // for test
+        }),
       })
-    );
+      .pipe(
+        map((responseData) => {
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
+        })
+      );
   }
 
   deletePost(url: string) {
