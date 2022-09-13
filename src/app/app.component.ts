2dce3e0fd786285ca61762d79d10d5b0c94b7047
A7Aima <aimaabhilash@gmail.com>
2022-09-13 11:00:28 +0530

    using pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +10,43 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPost();
+  }
 
+  url: string = 'https://http-angular-test-c4fd2-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post(this.url, postData).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
+    this.fetchPost();
     // Send Http request
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPost() {
+    this.http.get(this.url)
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postArray;
+      }))
+      .subscribe(body => {
+        console.log(body);
+      });
   }
 }
